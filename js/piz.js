8f980fec794b09daa53b867c64215419b294b3fc
ruth <ruthwanjiramugo@gmail.com>
2020-03-02 04:38:28 +0300

    add the js function on the checkout button

--- js/piz.js
+++ js/piz.js
@@ -1,1 +1,20 @@
-$('.checkout').fadeOut(fadeTime);
+$(function(){
+    var btnCheckout = document.getElementById('checkout');
+    function type(){
+        hover =document.getElementById ("grind-hover-price").value
+        alert("hi "+" your price is  "+" "+hover +"")
+    };
+    
+
+});
+function type(){
+    hover =document.getElementById ("grind-hover-price").value
+    alert("hi "+" your price is  "+" "+hover +"")
+};
+
+
+
+
+
+
+// $('.checkout').fadeOut(fadeTime);
